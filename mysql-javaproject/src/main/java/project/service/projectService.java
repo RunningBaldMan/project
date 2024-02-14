5ae6f33aaae08d6d9db951c5f54168b0c548c732
RunningBadlMan <150305321+RunningBaldMan@users.noreply.github.com>
2024-02-13 20:12:06 -0500

    week 11

--- mysql-javaproject/src/main/java/project/service/projectService.java
+++ mysql-javaproject/src/main/java/project/service/projectService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import project.dao.ProjectDao;
+import project.exception.DbException;
 import projects.entity.Project;
 
 public class projectService {
@@ -20,6 +21,20 @@ public class projectService {
 	public Project fetchProjectById(Integer projectId) {
 		return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException(
 				"Project with project ID=" + projectId + " does not exist."));
+	}
+
+	public void modifyProjectDetails(Project project) {
+		if(!projectDao.modifyProjectDetails(project)) {
+			throw new DbException("Project with ID=" + project.getProjectId() + " does not exist.");
+		}
+		
+		
+	}
+
+	public void deleteProject(Integer projectId) {
+		if(!projectDao.deleteProject(projectId)) {
+			throw new DbException("Project with ID=" + projectId + " does not existy.");
+		}
 	}
 
 }
